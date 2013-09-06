adbbd928dbcf902578cf5c2adb867085c3efb88e
Tom van Zummeren <tom.van.zummeren@gmail.com>
2013-09-06 17:08:27 +0200

    Added example of configuring sandbox/production

--- BrightCenterSDK/AppDelegate.m
+++ BrightCenterSDK/AppDelegate.m
@@ -9,12 +9,15 @@
 #import "AppDelegate.h"
 #import "MyCoolEduAppController.h"
 #import "BCStudentPickerController.h"
+#import "BCStudentsRepository.h"
 
 @implementation AppDelegate
 
 - (BOOL) application:(UIApplication *) application didFinishLaunchingWithOptions:(NSDictionary *) launchOptions {
     // In iOS7 this doesn't work. Instead each controller decides whether to hide the status bar
     application.statusBarHidden = YES;
+
+    [[BCStudentsRepository instance] configureForSandbox];
 
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
